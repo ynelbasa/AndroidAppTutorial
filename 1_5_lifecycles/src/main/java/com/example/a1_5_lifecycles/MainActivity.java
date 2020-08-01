129624ad97fa18f1935fbd3b2d1d88349a3db1bf
ynelbasa <marinelbasa@gmail.com>
2020-08-02 08:51:28 +1200

    Add logging for activity lifecycle investigation

--- 1_5_lifecycles/src/main/java/com/example/a1_5_lifecycles/MainActivity.java
+++ 1_5_lifecycles/src/main/java/com/example/a1_5_lifecycles/MainActivity.java
@@ -2,13 +2,67 @@ package com.example.a1_5_lifecycles;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    private static final String TAG = "MainActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button openSubActivity = findViewById(R.id.button);
+        openSubActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openSubActivity();            }
+        });
+
+        Log.i(TAG, "onCreate");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(TAG, "onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(TAG, "onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(TAG, "onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(TAG, "onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(TAG, "onDestroy");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.i(TAG, "onRestart");
+    }
+
+    public void openSubActivity(){
+        Intent intent = new Intent(this, SubActivity.class);
+        startActivity(intent);
     }
 }
