d7aecf2771268f9b164bb394ecab50f2f2b787c4
ynelbasa <marinelbasa@gmail.com>
2020-08-02 15:48:40 +1200

    Add intent animation library with spinner selection

--- 1_7_intentanimation/src/main/java/com/example/a1_7_intentanimation/MainActivity.java
+++ 1_7_intentanimation/src/main/java/com/example/a1_7_intentanimation/MainActivity.java
@@ -6,16 +6,22 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Spinner;
+
+import maes.tech.intentanim.CustomIntent;
 
 public class MainActivity extends AppCompatActivity {
     // Declare UI Elements
     private Button button;
+    private Spinner spinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         button = (Button)findViewById(R.id.button);
+        spinner = findViewById(R.id.spinner);
+
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -27,5 +33,6 @@ public class MainActivity extends AppCompatActivity {
     public void openSubActivity(){
         Intent intent = new Intent(this, SubActivity.class);
         startActivity(intent);
+        CustomIntent.customType(this, spinner.getSelectedItem().toString());
     }
 }
--- 1_7_intentanimation/src/main/java/com/example/a1_7_intentanimation/SubActivity.java
+++ 1_7_intentanimation/src/main/java/com/example/a1_7_intentanimation/SubActivity.java
@@ -4,11 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import maes.tech.intentanim.CustomIntent;
+
 public class SubActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sub);
+    }
+
+    @Override
+    public void finish() {
+        super.finish();
+        CustomIntent.customType(this, "fadein-to-fadeout");
     }
 }
