b17dafa82ecafbe2797b85db7191c10ef88846f0
ynelbasa <marinelbasa@gmail.com>
2020-08-06 08:00:52 +1200

    Initial project files for reusable views and layout app

---
+++ 2_3_includemerge/src/main/java/com/example/a2_3_includemerge/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.a2_3_includemerge;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
