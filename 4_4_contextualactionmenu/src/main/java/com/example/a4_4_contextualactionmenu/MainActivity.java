bdee6571ca9834272d307513fa223c155d3c9866
ynelbasa <marinelbasa@gmail.com>
2020-08-08 05:55:18 +1200

    Initial project files for adding context menu in action bar

---
+++ 4_4_contextualactionmenu/src/main/java/com/example/a4_4_contextualactionmenu/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.a4_4_contextualactionmenu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
