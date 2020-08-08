082bc5040bc6316577b07ac70194efdf19aa692d
ynelbasa <marinelbasa@gmail.com>
2020-08-08 14:38:13 +1200

    Initial project files for navigation drawer app

--- 4_5_bottomnavigationview/src/main/java/com/example/a4_5_bottomnavigationview/MainActivity.java
+++ 4_5_bottomnavigationview/src/main/java/com/example/a4_5_bottomnavigationview/MainActivity.java
@@ -15,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
+
         BottomNavigationView bottomNav = findViewById(R.id.bottom_nav);
         bottomNav.setOnNavigationItemSelectedListener(navListener);
 
