6b3bb2de321ec700f41628dd3c211a3e61bb3059
ynelbasa <marinelbasa@gmail.com>
2020-08-09 05:55:53 +1200

    Add hamburger menu and make status bar transparent

--- 4_6_navigationdrawer/src/main/java/com/example/a4_6_navigationdrawer/MainActivity.java
+++ 4_6_navigationdrawer/src/main/java/com/example/a4_6_navigationdrawer/MainActivity.java
@@ -1,11 +1,15 @@
 package com.example.a4_6_navigationdrawer;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    private DrawerLayout drawer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,5 +19,21 @@ public class MainActivity extends AppCompatActivity {
         // Use toolbar as action bar
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-}
+
+        drawer = findViewById(R.id.drawer_layout);
+        // String resources for accessibility
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
+                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
 }
