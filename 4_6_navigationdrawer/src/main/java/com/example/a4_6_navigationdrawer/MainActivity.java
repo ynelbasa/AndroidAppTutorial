25055997e80a6c78d2e2cf2be7f7d39d7311f7aa
ynelbasa <marinelbasa@gmail.com>
2020-08-09 05:31:55 +1200

    Use tool bar as action bar

--- 4_6_navigationdrawer/src/main/java/com/example/a4_6_navigationdrawer/MainActivity.java
+++ 4_6_navigationdrawer/src/main/java/com/example/a4_6_navigationdrawer/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.a4_6_navigationdrawer;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
@@ -10,5 +11,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+
+        // Use toolbar as action bar
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+}
 }
