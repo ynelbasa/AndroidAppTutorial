e1f43b579ee0ad618f0e46ac4ddec621c38faf79
ynelbasa <marinelbasa@gmail.com>
2020-08-09 17:37:36 +1200

    Apply alternative layout using constraint sets and apply transitions

--- 2_9_constraintsets/src/main/java/com/example/a2_9_constraintsets/MainActivity.java
+++ 2_9_constraintsets/src/main/java/com/example/a2_9_constraintsets/MainActivity.java
@@ -1,14 +1,50 @@
 package com.example.a2_9_constraintsets;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.constraintlayout.widget.ConstraintSet;
 
 import android.os.Bundle;
+import android.transition.ChangeBounds;
+import android.transition.Transition;
+import android.transition.TransitionManager;
+import android.view.View;
+import android.view.animation.OvershootInterpolator;
 
 public class MainActivity extends AppCompatActivity {
+    private ConstraintLayout layout;
+    // ConstraintSet hold constraints only not attributes
+    private ConstraintSet constraintSetOld = new ConstraintSet();
+    private ConstraintSet constraintSetNew = new ConstraintSet();
+    private boolean altLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        layout = findViewById(R.id.layout);
+
+        // Set default layout
+        constraintSetOld.clone(layout);
+        constraintSetNew.clone(this, R.layout.activity_main_alt);
+    }
+
+    public void swapView(View v) {
+        // Add custom transition
+        Transition changeBounds = new ChangeBounds();
+        changeBounds.setInterpolator(new OvershootInterpolator());
+
+        // Apply transitions
+        TransitionManager.beginDelayedTransition(layout, changeBounds);
+        if (!altLayout) {
+            // Apply alternative layout
+            constraintSetNew.applyTo(layout);
+            altLayout = true;
+        } else {
+            // Apply original layout
+            constraintSetOld.applyTo(layout);
+            altLayout = false;
+        }
     }
 }
