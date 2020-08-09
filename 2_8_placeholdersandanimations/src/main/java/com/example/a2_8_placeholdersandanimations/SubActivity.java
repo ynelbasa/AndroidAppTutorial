53cdcbdd02ac2588081acf43cb3382f0b42784fc
ynelbasa <marinelbasa@gmail.com>
2020-08-09 15:50:11 +1200

    Add reference to material.io and another demo for placeholder using button and image

---
+++ 2_8_placeholdersandanimations/src/main/java/com/example/a2_8_placeholdersandanimations/SubActivity.java
@@ -0,0 +1,14 @@
+package com.example.a2_8_placeholdersandanimations;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SubActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sub);
+    }
+}
