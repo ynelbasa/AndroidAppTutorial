1813a88ff7abfa651e800500ffb28411556bfa48
ynelbasa <marinelbasa@gmail.com>
2020-08-10 05:34:59 +1200

    Initial projects files for Gson tutorial app

---
+++ 5_3_gson/src/main/java/com/example/a5_3_gson/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.a5_3_gson;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
