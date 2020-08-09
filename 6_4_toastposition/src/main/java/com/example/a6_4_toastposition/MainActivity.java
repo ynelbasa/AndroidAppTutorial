ae9e2a99151f298b70c6829ade522d9449393c8c
ynelbasa <marinelbasa@gmail.com>
2020-08-10 07:32:29 +1200

    Changes toast position using setGravity

--- 6_4_toastposition/src/main/java/com/example/a6_4_toastposition/MainActivity.java
+++ 6_4_toastposition/src/main/java/com/example/a6_4_toastposition/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.a6_4_toastposition;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void showToast(View v) {
+        Toast toast = Toast.makeText(this, "Toast Message", Toast.LENGTH_SHORT);
+        toast.setGravity(Gravity.CENTER_VERTICAL | Gravity.START, 90, 0);
+        toast.show();
     }
 }
