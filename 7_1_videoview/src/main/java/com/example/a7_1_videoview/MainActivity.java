7c7d95e501811c80d75aea6c40d2c5320df72b32
ynelbasa <marinelbasa@gmail.com>
2020-08-30 13:44:53 +1200

    Add video view to play sample video from a folder

--- 7_1_videoview/src/main/java/com/example/a7_1_videoview/MainActivity.java
+++ 7_1_videoview/src/main/java/com/example/a7_1_videoview/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.a7_1_videoview;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.MediaController;
+import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        VideoView videoView = findViewById(R.id.video_view);
+        String videoPath = "android.resource://" + getPackageName() + "/" + R.raw.video;
+        Uri uri = Uri.parse(videoPath);
+        videoView.setVideoURI(uri);
+
+        // For button to control media
+        MediaController mediaController = new MediaController(this);
+        videoView.setMediaController(mediaController);
+
+        // Know where to be positioned on screen, parent of videoView
+        mediaController.setAnchorView(videoView);
     }
 }
