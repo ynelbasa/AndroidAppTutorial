979ec7a0cee6ac15daa103f5f509a92c57c95854
Ynel Basa <marinelbasa@gmail.com>
2020-09-16 10:27:28 +1200

    Implement adding of grocery list item using db cursor

---
+++ 9_grocerylist/src/main/java/com/apps/a9_grocerylist/GroceryAdapter.java
@@ -0,0 +1,75 @@
+package com.apps.a9_grocerylist;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+// Used to display groceryList to the UI
+public class GroceryAdapter extends RecyclerView.Adapter<GroceryAdapter.GroceryViewHolder> {
+    private Context mContext;
+    // Cursor gets the data out of the db
+    private Cursor mCursor;
+
+    public GroceryAdapter(Context context, Cursor cursor) {
+        mContext = context;
+        mCursor = cursor;
+    }
+
+    public class GroceryViewHolder extends RecyclerView.ViewHolder {
+        public TextView nameText;
+        public TextView countText;
+
+        public GroceryViewHolder(View itemView) {
+            super(itemView);
+            nameText = itemView.findViewById(R.id.textView_name_item);
+            countText = itemView.findViewById(R.id.textView_amount_item);
+        }
+    }
+
+    @Override
+    // Attach view from a resource grocery_item.xml
+    public GroceryViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(mContext);
+        View view = inflater.inflate(R.layout.grocery_item, parent, false);
+        return new GroceryViewHolder(view);
+    }
+
+    @Override
+    // Bind data in the grocery_item
+    public void onBindViewHolder(GroceryViewHolder holder, int position) {
+        // Check if cursor exists
+        if (!mCursor.moveToPosition(position)) {
+            return;
+        }
+
+        // Read columns from the database
+        String name = mCursor.getString(mCursor.getColumnIndex(GroceryContract.GroceryEntry.COLUMN_NAME));
+        int amount = mCursor.getInt(mCursor.getColumnIndex(GroceryContract.GroceryEntry.COLUMN_AMOUNT));
+
+        // Set the values into the UI
+        holder.nameText.setText(name);
+        holder.countText.setText(String.valueOf(amount));
+    }
+
+    @Override
+    public int getItemCount() {
+        return mCursor.getCount();
+    }
+
+    public void swapCursor(Cursor newCursor) {
+        if (mCursor != null) {
+            mCursor.close();
+        }
+
+        mCursor = newCursor;
+        if (newCursor != null) {
+            // Update recyclerview
+            notifyDataSetChanged();
+        }
+    }
+}
--- 9_grocerylist/src/main/java/com/apps/a9_grocerylist/MainActivity.java
+++ 9_grocerylist/src/main/java/com/apps/a9_grocerylist/MainActivity.java
@@ -1,14 +1,114 @@
 package com.apps.a9_grocerylist;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private SQLiteDatabase mDatabase;
+    private GroceryAdapter mAdapter;
+    private EditText mEditTextName;
+    private TextView mTextViewAmount;
+    private int mAmount = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Setup SqlLite Db
+        GroceryDbHelper dbHelper = new GroceryDbHelper(this);
+        // GetWritableDatabase because we want to add / update values into the db
+        mDatabase = dbHelper.getWritableDatabase();
+
+        // Get the UI for grocery list
+        RecyclerView recyclerView = findViewById(R.id.recyclerview);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        mAdapter = new GroceryAdapter(this, getAllItems());
+        recyclerView.setAdapter(mAdapter);
+
+        // Get UI elements
+        mEditTextName = findViewById(R.id.editText_name);
+        mTextViewAmount = findViewById(R.id.textView_amount);
+        Button buttonIncrease = findViewById(R.id.button_increase);
+        Button buttonDecrease = findViewById(R.id.button_decrease);
+        Button buttonAdd = findViewById(R.id.button_add);
+
+        // Set onclicklistener for increase, decrease and add button
+        buttonIncrease.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                increaseAmount();
+            }
+        });
+        buttonDecrease.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                decreaseAmount();
+            }
+        });
+        buttonAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addItem();
+            }
+        });
+    }
+
+    private void increaseAmount() {
+        mAmount++;
+        mTextViewAmount.setText(String.valueOf(mAmount));
+    }
+
+
+    private void decreaseAmount() {
+        if (mAmount > 0) {
+            mAmount--;
+            mTextViewAmount.setText(String.valueOf(mAmount));
+        }
+    }
+
+    // Add Item to SqlLite Db
+    private void addItem() {
+        // Do not save when amount or text is empty
+        if (mEditTextName.getText().toString().trim().length() == 0 || mAmount == 0) {
+            return;
+        }
+
+        String name = mEditTextName.getText().toString();
+
+        ContentValues cv = new ContentValues();
+        // Add column name for each associated value
+        cv.put(GroceryContract.GroceryEntry.COLUMN_NAME, name);
+        cv.put(GroceryContract.GroceryEntry.COLUMN_AMOUNT, mAmount);
+
+        mDatabase.insert(GroceryContract.GroceryEntry.TABLE_NAME, null, cv);
+
+        // Create new cursor or close existing cursor
+        mAdapter.swapCursor(getAllItems());
+
+        mEditTextName.getText().clear();
+    }
+
+    // Get all the items from grocery table
+    private Cursor getAllItems() {
+        return mDatabase.query(
+                GroceryContract.GroceryEntry.TABLE_NAME,
+                null,
+                null,
+                null,
+                null,
+                null,
+                GroceryContract.GroceryEntry.COLUMN_TIMESTAMP + " DESC"
+        );
     }
 }
