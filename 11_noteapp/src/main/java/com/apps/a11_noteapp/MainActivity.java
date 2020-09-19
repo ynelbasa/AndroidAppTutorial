5b72daa4470d172510ec8feb6ca3eda15516bd15
Ynel Basa <marinelbasa@gmail.com>
2020-09-19 21:08:31 +1200

    Add the view model and integrate to the activity

--- 11_noteapp/src/main/java/com/apps/a11_noteapp/MainActivity.java
+++ 11_noteapp/src/main/java/com/apps/a11_noteapp/MainActivity.java
@@ -1,14 +1,32 @@
 package com.apps.a11_noteapp;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.lifecycle.ViewModelProviders;
 
 import android.os.Bundle;
+import android.widget.Toast;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    private NoteViewModel noteViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        noteViewModel = new ViewModelProvider(this).get(NoteViewModel.class);
+        // LiveData is lifecycle aware, it automatically clean up resources preventing memory leaks
+        noteViewModel.getAllNotes().observe(this, new Observer<List<Note>>() {
+            @Override
+            public void onChanged(@Nullable List<Note> notes) {
+                //update RecyclerView
+                Toast.makeText(MainActivity.this, "onChanged", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
---
+++ 11_noteapp/src/main/java/com/apps/a11_noteapp/NoteViewModel.java
@@ -0,0 +1,41 @@
+package com.apps.a11_noteapp;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+
+import java.util.List;
+
+public class NoteViewModel extends AndroidViewModel {
+    private NoteRepository repository;
+    private LiveData<List<Note>> allNotes;
+
+    public NoteViewModel(@NonNull Application application) {
+        super(application);
+        repository = new NoteRepository(application);
+        allNotes = repository.getAllNotes();
+    }
+
+    // Wrapper for repository methods since the activity can access only the vm
+    public void insert(Note note) {
+        repository.insert(note);
+    }
+
+    public void update(Note note) {
+        repository.update(note);
+    }
+
+    public void delete(Note note) {
+        repository.delete(note);
+    }
+
+    public void deleteAllNotes() {
+        repository.deleteAllNotes();
+    }
+
+    public LiveData<List<Note>> getAllNotes() {
+        return allNotes;
+    }
+}
