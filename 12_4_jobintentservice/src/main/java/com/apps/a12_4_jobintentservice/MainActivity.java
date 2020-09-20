9eb3f499639ef71e385eb40538c5764c0252b9d9
Ynel Basa <marinelbasa@gmail.com>
2020-09-20 22:12:27 +1200

    Implement JobIntentService app

--- 12_4_jobintentservice/src/main/java/com/apps/a12_4_jobintentservice/MainActivity.java
+++ 12_4_jobintentservice/src/main/java/com/apps/a12_4_jobintentservice/MainActivity.java
@@ -2,13 +2,28 @@ package com.apps.a12_4_jobintentservice;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText editTextInput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        editTextInput = findViewById(R.id.edit_text_input);
+    }
+
+    public void enqueueWork(View v) {
+        String input = editTextInput.getText().toString();
+
+        Intent serviceIntent = new Intent(this, ExampleJobIntentService.class);
+        serviceIntent.putExtra("inputExtra", input);
+
+        ExampleJobIntentService.enqueueWork(this, serviceIntent);
     }
 }
