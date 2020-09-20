f76ddf57d4d39c019afe115d5bd553363db1136d
Ynel Basa <marinelbasa@gmail.com>
2020-09-21 01:14:22 +1200

    Implement ordered broadcast

--- 12_9_broadcastsender/src/main/java/com/apps/a12_9_broadcastsender/MainActivity.java
+++ 12_9_broadcastsender/src/main/java/com/apps/a12_9_broadcastsender/MainActivity.java
@@ -2,13 +2,27 @@ package com.apps.a12_9_broadcastsender;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView = findViewById(R.id.text_view);
+    }
+
+    public void sendBroadcast(View v) {
+        Intent intent = new Intent("EXAMPLE_ACTION");
+        intent.setPackage("com.apps.a12_9_orderedbroadcast");
+        Bundle extras = new Bundle();
+        extras.putString("stringExtra", "Start");
+        sendOrderedBroadcast(intent, null, new SenderReceiver(),
+                null, 0, "Start", extras);
     }
 }
