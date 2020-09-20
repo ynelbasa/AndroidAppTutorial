dd233c2a94984076ea1d26455771486ea5307e59
Ynel Basa <marinelbasa@gmail.com>
2020-09-20 20:00:32 +1200

    Initial project files for IntentService app

---
+++ 12_3_intentservice/src/main/java/com/apps/a12_3_intentservice/MainActivity.java
@@ -0,0 +1,14 @@
+package com.apps.a12_3_intentservice;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
