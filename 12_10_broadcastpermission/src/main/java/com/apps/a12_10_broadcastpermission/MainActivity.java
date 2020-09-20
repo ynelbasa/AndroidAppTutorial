3953fa406f6b1e6512a460f5d10264db69f1a77c
Ynel Basa <marinelbasa@gmail.com>
2020-09-21 01:27:40 +1200

    Add broadcast permission

--- 12_10_broadcastpermission/src/main/java/com/apps/a12_10_broadcastpermission/MainActivity.java
+++ 12_10_broadcastpermission/src/main/java/com/apps/a12_10_broadcastpermission/MainActivity.java
@@ -1,14 +1,27 @@
 package com.apps.a12_10_broadcastpermission;
 
+import android.Manifest;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.IntentFilter;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    OrderedReceiver1 orderedReceiver1 = new OrderedReceiver1();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        IntentFilter filter = new IntentFilter("EXAMPLE_ACTION");
+        filter.setPriority(1);
+        registerReceiver(orderedReceiver1, filter, Manifest.permission.VIBRATE, null);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        unregisterReceiver(orderedReceiver1);
     }
 }
--- 12_10_broadcastsender/src/main/java/com/apps/a12_10_broadcastsender/MainActivity.java
+++ 12_10_broadcastsender/src/main/java/com/apps/a12_10_broadcastsender/MainActivity.java
@@ -2,13 +2,28 @@ package com.apps.a12_10_broadcastsender;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView = findViewById(R.id.text_view);
+    }
+
+    public void sendBroadcast(View v) {
+        Intent intent = new Intent("EXAMPLE_ACTION");
+        intent.setPackage("com.apps.a12_10_broadcastpermission");
+        Bundle extras = new Bundle();
+        extras.putString("stringExtra", "Start");
+        sendOrderedBroadcast(intent, Manifest.permission.WAKE_LOCK, new SenderReceiver(),
+                null, 0, "Start", extras);
     }
 }
