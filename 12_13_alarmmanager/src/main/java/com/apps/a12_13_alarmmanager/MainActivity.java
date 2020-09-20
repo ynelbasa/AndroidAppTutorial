a93383d5063a2fb78e06e94c3111999c00fb9474
Ynel Basa <marinelbasa@gmail.com>
2020-09-21 01:46:33 +1200

    Initial project files for alarm manager

---
+++ 12_13_alarmmanager/src/main/java/com/apps/a12_13_alarmmanager/MainActivity.java
@@ -0,0 +1,14 @@
+package com.apps.a12_13_alarmmanager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
