6ad00297a909f005ce001d0e0570472c3c9f1da2
Ynel Basa <marinelbasa@gmail.com>
2020-09-21 00:31:10 +1200

    Implement explicit broadcast app

--- 12_8_broadcastsender/src/main/java/com/apps/a12_8_broadcastsender/MainActivity.java
+++ 12_8_broadcastsender/src/main/java/com/apps/a12_8_broadcastsender/MainActivity.java
@@ -2,13 +2,37 @@ package com.apps.a12_8_broadcastsender;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ComponentName;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView = findViewById(R.id.text_view);
+    }
+
+    public void sendBroadcast(View v) {
+        // Explicit broadcast
+        Intent intent = new Intent("EXAMPLE_ACTION");
+        PackageManager packageManager = getPackageManager();
+
+        List<ResolveInfo> infos = packageManager.queryBroadcastReceivers(intent, 0);
+        for (ResolveInfo info : infos) {
+            ComponentName cn = new ComponentName(info.activityInfo.packageName,
+                    info.activityInfo.name);
+            intent.setComponent(cn);
+            sendBroadcast(intent);
+        }
     }
 }
