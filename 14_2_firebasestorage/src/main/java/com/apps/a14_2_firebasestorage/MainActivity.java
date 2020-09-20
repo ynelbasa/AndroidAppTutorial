ed4591dc3400c0ac1d8b8b64ffc36c693006ac34
Ynel Basa <marinelbasa@gmail.com>
2020-09-21 02:51:31 +1200

    Initial project files for firebase storage

---
+++ 14_2_firebasestorage/src/main/java/com/apps/a14_2_firebasestorage/MainActivity.java
@@ -0,0 +1,14 @@
+package com.apps.a14_2_firebasestorage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
