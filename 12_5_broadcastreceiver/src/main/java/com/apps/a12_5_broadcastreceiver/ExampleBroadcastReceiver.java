e07f6754bc3c86d52212c8dfdb539d26d6bbf9f8
Ynel Basa <marinelbasa@gmail.com>
2020-09-20 22:51:29 +1200

    Add static receivers

--- 12_5_broadcastreceiver/src/main/java/com/apps/a12_5_broadcastreceiver/ExampleBroadcastReceiver.java
+++ 12_5_broadcastreceiver/src/main/java/com/apps/a12_5_broadcastreceiver/ExampleBroadcastReceiver.java
@@ -1,4 +1,20 @@
 package com.apps.a12_5_broadcastreceiver;
 
-public class ExampleBroadcastReceiver {
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.widget.Toast;
+
+// Execute code when the device boots up,connectivity changes, receive an SMS, etc
+public class ExampleBroadcastReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if (Intent.ACTION_BOOT_COMPLETED.equals(intent.getAction())) {
+            Toast.makeText(context, "Boot completed", Toast.LENGTH_SHORT).show();
+        }
+        if (ConnectivityManager.CONNECTIVITY_ACTION.equals(intent.getAction())) {
+            Toast.makeText(context, "Connectivity changed", Toast.LENGTH_SHORT).show();
+        }
+    }
 }
