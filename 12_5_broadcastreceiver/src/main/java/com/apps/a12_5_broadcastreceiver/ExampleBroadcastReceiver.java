2f9fe85dbd2d8e172a8dd5f2e660b8cb54212f31
Ynel Basa <marinelbasa@gmail.com>
2020-09-20 22:58:29 +1200

    Implement dynamic broadcast receiver

--- 12_5_broadcastreceiver/src/main/java/com/apps/a12_5_broadcastreceiver/ExampleBroadcastReceiver.java
+++ 12_5_broadcastreceiver/src/main/java/com/apps/a12_5_broadcastreceiver/ExampleBroadcastReceiver.java
@@ -7,14 +7,32 @@ import android.net.ConnectivityManager;
 import android.widget.Toast;
 
 // Execute code when the device boots up,connectivity changes, receive an SMS, etc
+
+
 public class ExampleBroadcastReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
+        /*
+        // Static Receiver
         if (Intent.ACTION_BOOT_COMPLETED.equals(intent.getAction())) {
             Toast.makeText(context, "Boot completed", Toast.LENGTH_SHORT).show();
         }
+
+        // Deprecated in v24 and above
         if (ConnectivityManager.CONNECTIVITY_ACTION.equals(intent.getAction())) {
             Toast.makeText(context, "Connectivity changed", Toast.LENGTH_SHORT).show();
+        }
+        */
+
+        if (ConnectivityManager.CONNECTIVITY_ACTION.equals(intent.getAction())) {
+            boolean noConnectivity = intent.getBooleanExtra(
+                    ConnectivityManager.EXTRA_NO_CONNECTIVITY, false
+            );
+            if (noConnectivity) {
+                Toast.makeText(context, "Disconnected", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(context, "Connected", Toast.LENGTH_SHORT).show();
+            }
         }
     }
 }
