1419448c25230d6482a80f0bd43c66b14188c63e
Ynel Basa <marinelbasa@gmail.com>
2020-09-21 02:32:33 +1200

    Initial project files for phone call app

---
+++ 13_4_phonecall/src/main/java/com/apps/a13_4_phonecall/MainActivity.java
@@ -0,0 +1,14 @@
+package com.apps.a13_4_phonecall;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
